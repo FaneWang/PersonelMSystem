d925404c0fe18f97b6bcf8bc6ebc56003108085e
FaneWang <2079409377@qq.com>
2018-04-18 22:37:34 +0800

    数据库类设计

--- PersonelMSystem/DataClass/DatabaseUtils.cs
+++ PersonelMSystem/DataClass/DatabaseUtils.cs
@@ -5,11 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using System.Xml.Linq;
 
 namespace PersonelMSystem.DataClass
 {
     class DatabaseUtils
     {
+        public string connStr;
+        public DatabaseUtils()
+        {
+            this.connStr = this.getConnStr();
+        }
 
+        // 获取数据库连接
+        public static SqlConnection getSqlConnection(string connStr)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connStr);
+            return sqlConnection;
+        }
+
+        #region 获取配置文件中的数据库连接字符串
+        private string getConnStr()
+        {
+            string configPath = System.Windows.Forms.Application.StartupPath + "\\database.xml";
+            XElement xe = XElement.Load(configPath);
+            var node = xe.Descendants("databaseConn").Where(xn => xn.Value.Contains("server")).Select(xn => xn.Value);
+            return node.FirstOrDefault<string>();
+        }
+        #endregion
     }
 }
